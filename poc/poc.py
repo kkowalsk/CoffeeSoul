2e3462f211320ebdff8678105f4f2dc442ba9d96
kevin <kevin.d.kowalski@gmail.com>
2026-06-30 16:42:19 -0600

    generate and load seed data (json)

--- poc/poc.py
+++ poc/poc.py
@@ -1,5 +1,6 @@
 import uuid
 import datetime
+import json
 
 class CoffeeComrade:
     def __init__(self, name):
@@ -56,21 +57,63 @@ class LineItem:
 
 
 def init():
-    print("hello world")
-    person = CoffeeComrade("kevin")
-    print(person)
-    cortado = Brew("cortado", 6.5, "1 part milk to 1 part espresso")
-    cold = Brew("cold brew coffee", 5, "cold water extracted")
-    print(cortado)
-    person.setBrew(cortado)
-    print(person)
-    lineItem1 = LineItem(person, cortado)
-    lineItem2 = LineItem(person, cold)
-    order = Order()
-    order.addLineItem(lineItem1)
-    order.addLineItem(lineItem2)
-    print(order)
-    print(order.calculateTotal())
+    # print("hello world")
+    # person = CoffeeComrade("kevin")
+    # print(person)
+    # cortado = Brew("cortado", 6.5, "1 part milk to 1 part espresso")
+    # cold = Brew("cold brew coffee", 5, "cold water extracted")
+    # print(cortado)
+    # person.setBrew(cortado)
+    # print(person)
+    # lineItem1 = LineItem(person, cortado)
+    # lineItem2 = LineItem(person, cold)
+    # order = Order()
+    # order.addLineItem(lineItem1)
+    # order.addLineItem(lineItem2)
+    # print(order)
+    # print(order.calculateTotal())
+
+    brews = []
+    comrades = []
+    orders = []
+    with open('./seedData/brews.json', 'r') as file:
+        data = json.load(file)
+        for b in data:
+            brews.append(Brew(b["name"], b["price"], b["description"]))
+            print(brews[-1])
+
+    brewsByName = {brew.name: brew for brew in brews}
+
+    with open('./seedData/comrades.json', 'r') as file:
+        data = json.load(file)
+        for c in data:
+            comrade = CoffeeComrade(c["name"])
+            comrade.setBrew(brewsByName[c["defaultBrewId"]])
+            comrades.append(comrade)
+            print(comrades[-1])
+
+    comradesByName = {comrade.name: comrade for comrade in comrades}
+
+    with open('./seedData/orders.json', 'r') as file:
+        data = json.load(file)
+        for o in data:
+            order = Order()
+            for li in o["lineItems"]:
+                lineItem = LineItem(comradesByName[li["comradeName"]], brewsByName[li["brewName"]])
+                order.addLineItem(lineItem)
+            orders.append(order)
+            print(orders[-1])
+            print(orders[-1].calculateTotal())
+
+    for comrade in comrades:
+        total = 0
+        for order in orders:
+            for lineItem in order.items:
+                if lineItem.coffeeComrade is comrade:
+                    total += lineItem.brew.price
+
+        print(comrade.name, total)
+
 
 if __name__ == "__main__":
     init()
