5ce87460c1fba51a9f9ebf93c588f760b0ba52b1
kevin <kevin.d.kowalski@gmail.com>
2026-07-01 20:49:21 -0600

    poc completed: smooth weighted round robin implemented considering running total across order line items
    3 test data files generated with 1000 line items each

--- jul_01_26__prompts.md
+++ jul_01_26__prompts.md
@@ -4,3 +4,12 @@ java. off the shelf credentials management in containerized microservice environ
 * is HashiCorp Vault or Infisical free for use for small deployments?
 * which has the simple configuration/deployment/management for a self-hosted side project
 
+python sorted collection based on specific field in the class. sorted upon insert?
+[... collection of basic python questions]
+
+override the orders_repeatative.json file so that it contains 1000 copies of the following json in a list
+[json string ...]
+
+expand orders_variable.json with 1000 entries of random orders
+
+ duplicate orders_variable.json into a separate file but 50% of the time, they order their regular drink which may be found in comrades.json
--- poc/poc.py
+++ poc/poc.py
@@ -1,22 +1,23 @@
+from __future__ import annotations
 import uuid
 import datetime
 import json
+import sortedcontainers
 
 class CoffeeComrade:
-    def __init__(self, name):
+    def __init__(self, name: str, brew: Brew = None):
         self.uuid = uuid.uuid4()
         self.name = name
-        self.brew = None
+        self.brew = brew
 
     def __str__(self):
         return f'{self.uuid}: {self.name} { 'drinkin\' ' + self.brew.name if self.brew is not None else ''}'
 
-    def setBrew(self, brew):
+    def setBrew(self, brew: Brew):
         self.brew = brew
 
-
 class Brew: 
-    def __init__(self, name, price, description):
+    def __init__(self, name: str, price: float, description: str):
         self.uuid = uuid.uuid4()
         self.name = name
         self.description = description
@@ -30,31 +31,85 @@ class Order:
         self.uuid = uuid.uuid4()
         self.timestamp = datetime.datetime.now()
         self.items = []
+        self.payee = None
 
     def __str__(self):
-        return f'{self.uuid}: {self.timestamp} - items:\n  ' + '\n  '.join([str(item) for item in self.items])
+        return f'{self.uuid}: {self.timestamp}. payee {self.payee}. items:\n  ' + '\n  '.join([str(item) for item in self.items])
 
-    def addLineItem(self, lineItem):
+    def addLineItem(self, lineItem: LineItem):
         self.items.append(lineItem)
 
-    def removeLineItem(self, lineItem):
+    def removeLineItem(self, lineItem: LineItem):
         self.items.remove(lineItem)
 
-    def calculateTotal(self):
+    def calculateTotal(self) -> int:
         runningTotal = 0
         for item in self.items:
             runningTotal += item.brew.price
         return runningTotal
 
+    def setPayee(self, payee: CoffeeComrade):
+        self.payee = payee
+
 class LineItem:
-    def __init__(self, coffeeComrade, brew):
+    def __init__(self, comrade: CoffeeComrade, brew: Brew):
         self.uuid = uuid.uuid4()
         self.brew = brew
-        self.coffeeComrade = coffeeComrade
+        self.comrade = comrade
 
     def __str__(self):
-        return f'{self.uuid}: line item - {self.brew.name} for {self.coffeeComrade.name}'
+        return f'{self.uuid}: line item - {self.brew.name} for {self.comrade.name}'
 
+# inspiration pulled from here
+# https://oneuptime.com/blog/post/2026-01-30-weighted-round-robin/view
+class SmoothWeightedRoundRobin:
+    def __init__(self):
+        self.uuid = uuid.uuid4()
+        self.orders = sortedcontainers.SortedKeyList(key=lambda o: o.timestamp)
+        self.comradeWeights = dict()
+        self.comradeTotals = dict()
+        self.comradePayments = dict()
+        
+    # use to build up a history for testing
+    def add(self, order: Order):
+        self.orders.add(order)
+        for i in order.items:
+            self.comradeWeights.setdefault(i.comrade, 0)
+            self.comradeTotals.setdefault(i.comrade, 0)
+            self.comradePayments.setdefault(i.comrade, 0)
+
+    def calculateHistoricalPayees(self):
+        print('calculating historical payees')
+        overallTotal = 0;
+        for o in self.orders:
+            total = o.calculateTotal()
+            overallTotal += total
+
+            # print(f'{o}')
+            for li in o.items:
+                self.comradeWeights[li.comrade] += li.brew.price
+                self.comradeTotals[li.comrade] += li.brew.price
+                # print(f'{li}. new weight {self.comradeWeights[li.comrade]}')
+                # print(f'{li}. new total  {self.comradeTotals[li.comrade]}')
+
+
+            # print()
+            payee = None
+            for comrade, weight in self.comradeWeights.items():
+                if payee is None or weight > self.comradeWeights[payee]:
+                    payee = comrade
+            self.comradeWeights[payee] -= total
+            self.comradePayments[payee] += 1
+            # print(f'{payee.name}\'s new weight is {self.comradeWeights[payee]}')
+            o.setPayee(payee)
+            # print()
+        
+        for comrade in self.comradeWeights:
+            print(f'{comrade.name} total {self.comradeTotals[comrade]}, weight {self.comradeWeights[comrade]}, payments {self.comradePayments[comrade]}. total% {round(self.comradeTotals[comrade]/overallTotal, 3)} vs. payment% {self.comradePayments[comrade]/len(self.orders)}')
+
+
+    def __str__(self):
+        return "".join(f'{o}\n' for o in self.orders)
 
 def init():
     # print("hello world")
@@ -94,7 +149,7 @@ def init():
 
     comradesByName = {comrade.name: comrade for comrade in comrades}
 
-    with open('./seedData/orders.json', 'r') as file:
+    with open('./seedData/orders_mixed.json', 'r') as file:
         data = json.load(file)
         for o in data:
             order = Order()
@@ -102,17 +157,25 @@ def init():
                 lineItem = LineItem(comradesByName[li["comradeName"]], brewsByName[li["brewName"]])
                 order.addLineItem(lineItem)
             orders.append(order)
-            print(orders[-1])
-            print(orders[-1].calculateTotal())
+            # print(orders[-1])
+            # print(orders[-1].calculateTotal())
 
-    for comrade in comrades:
-        total = 0
-        for order in orders:
-            for lineItem in order.items:
-                if lineItem.coffeeComrade is comrade:
-                    total += lineItem.brew.price
+    # for comrade in comrades:
+    #     total = 0
+    #     for order in orders:
+    #         for lineItem in order.items:
+    #             if lineItem.comrade is comrade:
+    #                 total += lineItem.brew.price
 
-        print(comrade.name, total)
+        # print(comrade.name, total)
+
+    roundRobin = SmoothWeightedRoundRobin()
+    for o in reversed(orders):
+        roundRobin.add(o)
+    print("\nRoundRobin:")
+    print(roundRobin)
+    print()
+    roundRobin.calculateHistoricalPayees()
 
 
 if __name__ == "__main__":
